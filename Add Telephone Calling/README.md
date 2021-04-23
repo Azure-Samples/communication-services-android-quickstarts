74935e2f2721fd1922e400101d265a49955135e2
Ravi Thanneeru <v-ratann@microsoft.com>
2021-04-23 16:42:05 -0700

    Adding telephony calling quick start sample

---
+++ Add Telephone Calling/README.md
@@ -0,0 +1,54 @@
+---
+page_type: sample
+languages:
+- java
+products:
+- azure
+- azure-communication-services
+---
+
+# Quickstart: Add telephone calling to your Android app
+
+For full instructions on how to build this code sample from scratch, look at [Quickstart: Add telephone calling to your Android app](https://docs.microsoft.com/en-us/azure/communication-services/quickstarts/voice-video-calling/pstn-call?pivots=platform-android)
+
+## Prerequisites
+
+To complete this tutorial, you’ll need the following prerequisites:
+
+- An Azure account with an active subscription. [Create an account for free](https://azure.microsoft.com/free/?WT.mc_id=A261C142F). 
+- [Android Studio](https://developer.android.com/studio), for running your Android application.
+- A deployed Communication Services resource. [Create a Communication Services resource](https://docs.microsoft.com/en-us/azure/communication-services/quickstarts/create-communication-resource).
+- A [User Access Token](https://docs.microsoft.com/en-us/azure/communication-services/quickstarts/access-tokens?pivots=programming-language-csharp) for your Azure Communication Service.
+- A phone number acquired in Communication Services resource. [how to get a phone number](https://docs.microsoft.com/en-us/azure/communication-services/quickstarts/telephony-sms/get-phone-number).
+- Complete the quickstart for [getting started with adding calling to your application](https://docs.microsoft.com/en-us/azure/communication-services/quickstarts/voice-video-calling/getting-started-with-calling).
+## Code Structure
+
+- **./app/src/main/java/com/contoso/acsquickstart/MainActivity.java:** Contains core logic for calling SDK integration.
+- **./app/src/main/res/layout/activity_main.xml:** Contains core UI for sample app.
+
+## Object model
+
+The following classes and interfaces used in the quickstart handle some of the major features of the Azure Communication Services Calling client library:
+
+| Name                                  | Description                                                  |
+| ------------------------------------- | ------------------------------------------------------------ |
+| CallClient| The CallClient is the main entry point to the Calling client library.|
+| CallAgent | The CallAgent is used to start and manage calls. |
+| CommunicationTokenCredential  | The CommunicationTokenCredential  is used as the token credential to instantiate the CallAgent.|
+
+## Before running sample code
+
+1. Open an instance of PowerShell, Windows Terminal, Command Prompt or equivalent and navigate to the directory that you'd like to clone the sample to.
+2. `git clone https://github.com/Azure-Samples/Communication-Services-Android-Quickstarts.git` 
+3. With the `Access Token` procured in pre-requisites, add it to the **./app/src/main/java/com/contoso/acsquickstart/MainActivity.java** file. Assign your access token in line 30:
+   ```private static final String UserToken = "<User_Access_Token>";```
+3. With the `phone number` procured in pre-requisites, add it to the **./app/src/main/java/com/contoso/acsquickstart/MainActivity.java** file. Assign your ACS phone number in line 60:
+   ```PhoneNumberIdentifier callerPhone = new PhoneNumberIdentifier("ACS Phone number");```
+
+## Run the sample
+
+Open the sample project using Android Studio and run the application.
+
+![Final look and feel of the quick start app](../Media/quickstart-android-call-pstn.png)
+
+The app can now be launched using the "Run App" button on the toolbar (Shift+F10). You can make an call to phone by providing a phone number in the added text field and clicking the CALL button
---
+++ Add Telephone Calling/app/src/main/java/com/contoso/acsquickstart/MainActivity.java
@@ -0,0 +1,130 @@
+package com.contoso.acsquickstart;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+import com.azure.android.communication.calling.VideoOptions;
+import com.azure.android.communication.common.CommunicationUserIdentifier;
+import com.azure.android.communication.calling.Call;
+import com.azure.android.communication.calling.CallAgent;
+import com.azure.android.communication.calling.CallClient;
+import com.azure.android.communication.calling.HangUpOptions;
+import com.azure.android.communication.common.CommunicationTokenCredential;
+import com.azure.android.communication.calling.StartCallOptions;
+import com.azure.android.communication.common.PhoneNumberIdentifier;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String[] allPermissions = new String[] { Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_PHONE_STATE };
+    private static final String UserToken = "<User_Access_Token>";
+
+    TextView statusBar;
+
+    private CallAgent agent;
+    private Call call;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getAllPermissions();
+        createAgent();
+
+        Button callButton = findViewById(R.id.call_button);
+        callButton.setOnClickListener(l -> startCall());
+
+        Button hangupButton = findViewById(R.id.hangup_button);
+        hangupButton.setOnClickListener(l -> endCall());
+
+        statusBar = findViewById(R.id.status_bar);
+    }
+
+    /**
+     * Start a call
+     */
+    private void startCall() {
+       EditText calleePhoneView = findViewById(R.id.callee_id);
+        String calleePhone = calleePhoneView.getText().toString();
+        PhoneNumberIdentifier callerPhone = new PhoneNumberIdentifier("ACS Phone number");
+        StartCallOptions options = new StartCallOptions();
+        options.setAlternateCallerId(callerPhone);
+        options.setVideoOptions(new VideoOptions(null));
+        call = agent.startCall(
+                getApplicationContext(),
+                new PhoneNumberIdentifier[] {new PhoneNumberIdentifier(calleePhone)},
+                options);
+		call.addOnStateChangedListener(p -> setStatus(call.getState().toString()));
+    }
+
+    /**
+     * Ends the call previously started
+     */
+    private void endCall() {
+        try {
+            call.hangUp(new HangUpOptions()).get();
+        } catch (ExecutionException | InterruptedException e) {
+            Toast.makeText(this, "Unable to hang up call", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Create the call agent
+     */
+    private void createAgent() {
+        try {
+            CommunicationTokenCredential credential = new CommunicationTokenCredential(UserToken);
+            agent = new CallClient().createCallAgent(getApplicationContext(), credential).get();
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(), "Failed to create call agent.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Request each required permission if the app doesn't already have it.
+     */
+    private void getAllPermissions() {
+        ArrayList<String> permissionsToAskFor = new ArrayList<>();
+        for (String permission : allPermissions) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                permissionsToAskFor.add(permission);
+            }
+        }
+        if (!permissionsToAskFor.isEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsToAskFor.toArray(new String[0]), 1);
+        }
+    }
+
+    /**
+     * Ensure all permissions were granted, otherwise inform the user permissions are missing.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, int[] grantResults) {
+        boolean allPermissionsGranted = true;
+        for (int result : grantResults) {
+            allPermissionsGranted &= (result == PackageManager.PERMISSION_GRANTED);
+        }
+        if (!allPermissionsGranted) {
+            Toast.makeText(this, "All permissions are needed to make the call.", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    /**
+     * Shows message in the status bar
+     */
+    private void setStatus(String status) {
+        runOnUiThread(() -> statusBar.setText(status));
+    }
+}
