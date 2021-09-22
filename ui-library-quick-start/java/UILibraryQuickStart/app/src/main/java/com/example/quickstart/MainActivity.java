f51d2c96b9dcc13408f7e26ce37a1f2999171ee8
Inderpal Singh Aulakh <73618019+iaulakh@users.noreply.github.com>
2021-09-22 16:21:59 -0700

    UI Library Quick Start - DRAFT

---
+++ ui-library-quick-start/java/UILibraryQuickStart/app/src/main/java/com/example/quickstart/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.quickstart;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import com.azure.android.communication.common.CommunicationTokenCredential;
+import com.azure.android.communication.common.CommunicationTokenRefreshOptions;
+import com.azure.android.communication.toolkit.ACSCallingCompositeBuilder;
+import com.azure.android.communication.toolkit.CallingComposite;
+import com.azure.android.communication.toolkit.GroupCallParameters;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startCallingCompositeButton = findViewById(R.id.startUILibraryButton);
+
+        startCallingCompositeButton.setOnClickListener(l -> {
+            startCallingComposite();
+        });
+    }
+
+    private void startCallingComposite() {
+        CallingComposite callingComposite = new ACSCallingCompositeBuilder().build();
+
+        CommunicationTokenRefreshOptions communicationTokenRefreshOptions =
+                new CommunicationTokenRefreshOptions(this::fetchToken, true);
+        CommunicationTokenCredential communicationTokenCredential = new CommunicationTokenCredential(communicationTokenRefreshOptions);
+
+        GroupCallParameters parameters = new GroupCallParameters(this,
+                communicationTokenCredential,
+                "DISPLAY_NAME",
+                UUID.fromString("GROUP_CALL_ID"));
+
+        callingComposite.startExperience(parameters);
+    }
+
+    private String fetchToken() {
+        return "USER_ACCESS_TOKEN";
+    }
+}
---
+++ ui-library-quick-start/kotlin/UILibraryQuickStart/app/src/main/java/com/example/uilibraryquickstart/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.uilibraryquickstart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.azure.android.communication.common.CommunicationTokenCredential
+import com.azure.android.communication.common.CommunicationTokenRefreshOptions
+import com.azure.android.communication.toolkit.ACSCallingCompositeBuilder
+import com.azure.android.communication.toolkit.CallingComposite
+import com.azure.android.communication.toolkit.GroupCallParameters
+import java.util.UUID
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startCallingCompositeButton = findViewById<Button>(R.id.startUILibraryButton)
+
+        startCallingCompositeButton.setOnClickListener { l: View? -> startCallingComposite() }
+    }
+
+    private fun startCallingComposite() {
+        val callingComposite: CallingComposite = ACSCallingCompositeBuilder().build()
+        val communicationTokenRefreshOptions = CommunicationTokenRefreshOptions(
+            { fetchToken() }, true)
+        val communicationTokenCredential =
+            CommunicationTokenCredential(communicationTokenRefreshOptions)
+        val parameters = GroupCallParameters(this,
+            communicationTokenCredential,
+            "DISPLAY_NAME",
+            UUID.fromString("GROUP_CALL_ID"))
+        callingComposite.startExperience(parameters)
+    }
+
+    private fun fetchToken(): String? {
+        return "USER_ACCESS_TOKEN"
+    }
+}
